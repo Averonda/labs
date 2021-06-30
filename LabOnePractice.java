74cdf0daa31b16742bc80eebd6e3b7b6edc05986
Averonda <86671734+Averonda@users.noreply.github.com>
2021-06-30 17:46:32 -0600

    Add files via upload

---
+++ LabOnePractice.java
@@ -0,0 +1,28 @@
+package labOne;
+
+public class LabOnePractice {
+	public static void main(String[] args) {
+		System.out.println("Anthony Veronda");
+		
+		boolean on = false;
+		System.out.println(on);
+		
+		short s = 16;
+		System.out.println(s);
+		
+		int i = -32;
+		System.out.println(i);
+		
+		float f = 3839.34893F;
+		System.out.println(f);
+		
+		long l = 484161856386484861L;
+		System.out.println(l);
+		
+		char c = 'a';
+		System.out.println(c);
+		
+		
+		
+	}
+}
