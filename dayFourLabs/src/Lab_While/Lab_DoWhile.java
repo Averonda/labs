e334c67ee8bf55870ff0d71ed2746d157b1fd881
Averonda <86671734+Averonda@users.noreply.github.com>
2021-07-02 15:10:51 -0600

    Add files via upload

---
+++ dayFourLabs/src/Lab_While/Lab_DoWhile.java
@@ -0,0 +1,11 @@
+package Lab_While;
+
+public class Lab_DoWhile {
+	 public static void main(String args[]){
+	    	boolean on = false;
+	    	
+	    	do{
+	    		System.out.println("Inside the do-while loop");
+	    	} while (on);
+		}
+}
---
+++ dayFourLabs/src/Lab_While/Lab_While.java
@@ -0,0 +1,12 @@
+package Lab_While;
+
+public class Lab_While {
+	public static void main(String[] args) {
+		boolean on = true;
+
+        while (on){
+            System.out.println("Inside the while loop");
+            //on = false;
+        }
+	}
+}
